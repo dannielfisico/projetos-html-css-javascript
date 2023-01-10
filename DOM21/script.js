c4503a3cfeaceb19bb7bcf669e1c006d5e409910
dannielfisico <treinamentosinterativa@gmail.com>
2023-01-10 11:58:20 -0300

    atualização album

--- DOM21/script.js
+++ DOM21/script.js
@@ -1,15 +1,15 @@
 function postar() {
     const concordo = document.querySelector('input#concordo')
     if (concordo.checked){
-
-
-    const nomePontoTuristico = document.querySelector('input#nome').value
-    const descricao = document.querySelector('input#descricao').value
-    const url = document.querySelector('input#url').value
+    const nomePontoTuristico = document.querySelector('input#nome')
+    const descricao = document.querySelector('input#descricao')
+    const url = document.querySelector('input#url')
     const relacao = document.getElementsByName('rel')
     //criação dos elementos do Álbum dinamicamente
-    const album = document.querySelector('div#album')
-    album.setAttribute('style', 'border-style: solid')
+    const publicacao = document.querySelector('div#album')
+    // publicacao.setAttribute('style', 'border-style: solid')
+    const album = document.createElement('section')
+    publicacao.appendChild(album)
     const foto = document.createElement('img')
     album.appendChild(foto)
     const txtnome = document.createElement('p')
@@ -26,16 +26,25 @@ function postar() {
         } 
 
     foto.setAttribute('src', url)
-    txtnome.innerHTML = nomePontoTuristico
-    txtdescricao.innerHTML = descricao
+    txtnome.innerHTML = nomePontoTuristico.value
+    txtdescricao.innerHTML = descricao.value
     txtRelacao.innerHTML = suaRelacao
+/*
+    Apagar os dados dos campos do formuláro
+    e desmarcar a caixa concordo*/
+    nomePontoTuristico.value = ""
+    descricao.value = ""
+    url.value = ""
+    relacao[0].checked = true
+    concordo.checked = false
+    nomePontoTuristico.focus()  
 
+
+/*Caso a caixa Concordo não esteja marcada, irá aparecer esse alerta */
     } else {
         alert(`Concorde com os termos antes de postar!`)
     }
+     
     
-    
-
-
 
 }
