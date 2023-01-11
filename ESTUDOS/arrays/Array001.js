855e97871b7915420113cb3ea9869fe4df71856d
dannielfisico <treinamentosinterativa@gmail.com>
2023-01-11 12:41:16 -0300

    Estudos array

---
+++ ESTUDOS/arrays/Array001.js
@@ -0,0 +1,32 @@
+const array = [] /*Array vazio definido*/
+const a = 2
+console.log(array)
+array.push(a)  /*Variável 'a' adicionada no final do array */ 
+console.log(array)
+const b = 5
+array.push(b) /*Variável 'b' adicionada no final do array */ 
+console.log(array)
+const c = 3
+array.push(c) /*Variável 'c' adicionada no final do array */ 
+const d = 9
+array.push(d) /*Variável 'd' adicionada no final do array */ 
+const e = 0
+array.push(e) /*Variável 'e' adicionada no final do array */ 
+console.log(array) /*Impressão do array na mesma ordem em que os elementos foram adicionados */ 
+const arrayCres  = array.sort() /* O método .sort() faz com que os elementos do array sejam classificados em ordem crescente (ordem alfabética em caso de string) */
+console.log(arrayCres) /* Impressão do arrayCres que é o array normal em ordem crescente, ou seja os elementos são colocados do menor para o maior*/
+const menor = arrayCres[0] /*Menor valor contido no arrayCres*/
+const maior = arrayCres[arrayCres.length - 1] /*Maior valor contido no arrayCres*/
+console.log(`O menor e maior valor são respectivamente: ${menor} e ${maior}`)
+
+let soma = 0 
+//O valor da variável 'soma' tem que ser definido para que a váriável receba um tipo numérico, caso contrário ela fica definida no escopo com um tipo 'undefined' o que irá dá um erro na hora de somar, pois o primeiro termo da soma seria o valor contido inicialmente na variável soma.
+let total //só pra testar, vou imprimir o typeof dessa variável no console
+console.log(typeof soma) /* Impressão no console do tipo dessa variável */
+console.log(typeof total) /* Impressão no console do tipo dessa variável */
+
+/* Aqui usei um for para adicionar cada elemento do arrayCres dentro da variável soma que inicialmente vale 0 (zero) */
+for (let contador = 0; contador < arrayCres.length; contador++){
+    soma += arrayCres[contador]
+} /* O contador definido como 0 e enquanto o contador for menor que o tamanho do arrayCres, será incrementado 1 em cada interação e a soma recebe ela mesma + o valor contido em cada um dos elementos contidos no arrayCres em cada uma das interações.*/
+console.log(`A soma dos termos é ${soma}.`) 
---
+++ ESTUDOS/desafio-listas-info/script.js
@@ -0,0 +1,16 @@
+function add(){
+    const listaAdd = []
+    const numForm = Number(document.querySelector('input#num').value)
+    const listaHTML = document.querySelector('ul')
+    const opcao = document.createElement('li')
+    listaAdd.push() 
+    const listaCrescente =  listaAdd.sort()
+    opcao = listaAdd[0]
+    listaHTML.appendChild(opcao)
+}
+
+
+
+
+
+
