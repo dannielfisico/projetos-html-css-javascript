c0b9d4e0f6419c86323b4a132325d855f36c087b
dannielfisico <treinamentosinterativa@gmail.com>
2023-01-13 13:41:10 -0300

    Editor de fotos

--- ESTUDOS/manipulandoValores/scritp.js
+++ ESTUDOS/manipulandoValores/scritp.js
@@ -1,30 +1,51 @@
 const quadrado = document.querySelector('.quadrado')
-let cor1 = 0
-let cor2 = 0
-let cor3 = 0
+let cor1 = 0 //Canal R
+let cor2 = 0 //Canal G
+let cor3 = 0 //Canal B
+let alfa = 0 //canal A (alfa transparência em rgba)
+
 function mudarCor1(){
+    const cor2atual = cor2
+    const cor3atual = cor3
     const cor1Resp = document.querySelector('span#cor1')
     const cor1Seletor = document.querySelector('input#vermelho').value
     cor1Resp.innerHTML = `(${cor1Seletor})`
-    quadrado.setAttribute('style', 'background-color: rgb('+cor1Seletor+','+cor2+','+cor3+')')
-    cor1 = cor1Seletor.value
+    quadrado.setAttribute('style', 'background-color: rgba('+cor1Seletor+','+cor2atual+','+cor3atual+',0.500)')
+    cor1 = cor1Seletor
 }
 
 function mudarCor2(){
+    const cor1atual = cor1
+    const cor3atual = cor3
     const cor2Resp = document.querySelector('span#cor2')
     const cor2Seletor = document.querySelector('input#verde').value
     cor2Resp.innerHTML = `(${cor2Seletor})`
-    quadrado.setAttribute('style', 'background-color: rgb('+cor1+','+cor2Seletor+','+cor3+')')
-    cor2 = cor2Seletor.value
+    quadrado.setAttribute('style', 'background-color: rgba('+cor1atual+','+cor2Seletor+','+cor3atual+',0.500)')
+    cor2 = cor2Seletor
 }
 
 function mudarCor3(){
+    const cor1atual = cor1
+    const cor2atual = cor2
     const cor3Resp = document.querySelector('span#cor3')
     const cor3Seletor = document.querySelector('input#azul').value
     cor3Resp.innerHTML = `(${cor3Seletor})`
-    quadrado.setAttribute('style', 'background-color: rgb('+cor1+', '+cor2+','+cor3Seletor+')')
-    cor3 = cor3Seletor.value
+    quadrado.setAttribute('style', 'background-color: rgba('+cor1atual+', '+cor2atual+','+cor3Seletor+',0.500)')
+    cor3 = cor3Seletor
 }
+
+function mudarAlfa(){
+    const cor1atual = cor1
+    const cor2atual = cor2
+    const cor3atual = cor3
+    const alfaResp = document.querySelector('span#alfa')
+    const alfaSeletorValor = document.querySelector('input#alfa').value
+    const alfaSeletor = alfaSeletorValor/1000
+    alfaResp.innerHTML = `(${alfaSeletor})`
+    quadrado.setAttribute('style', 'background-color: rgba('+cor1atual+', '+cor2atual+','+cor3atual+','+ alfaSeletor +')')
+    alfa = alfaSeletor
+}
+
 
 
 
