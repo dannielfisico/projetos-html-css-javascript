d4057f9778236b302a3d47334d52b95cd48ee3be
dannielfisico <treinamentosinterativa@gmail.com>
2023-01-17 17:26:42 -0300

    arrays .indexOf()

---
+++ ESTUDOS/arrays/Array.prototype.indexOf.js
@@ -0,0 +1,7 @@
+/*O método indexOf() retorna o primeiro índice em que o elemento pode ser encontrado no array, retorna -1 caso o mesmo não esteja presente.
+*/
+
+const frutas = ['laranja', 'maçã', 'abacaxi','pêra']
+console.log(frutas.indexOf('maçã'))
+
+
