31aeba5e86862c2d8aa19c20ecaae59d28268c0e
dannielfisico <treinamentosinterativa@gmail.com>
2023-01-17 14:25:00 -0300

    arrays .push()

---
+++ ESTUDOS/arrays/Array.prototype.push.js
@@ -0,0 +1,7 @@
+/*O método push() adiciona um ou mais elementos ao final de um array e retorna o novo comprimento desse array.*/
+const frutas = ['laranja', 'maçã', 'abacaxi','pêra']
+
+frutas.push('tangerina')
+console.log(frutas)
+frutas.push('mamão','banana')
+console.log(frutas)
