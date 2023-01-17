466eca9821224c9bf71924e589e8d832d57ff74c
dannielfisico <treinamentosinterativa@gmail.com>
2023-01-17 14:18:31 -0300

    arrays .pop()

---
+++ ESTUDOS/arrays/Array.prototype.pop.js
@@ -0,0 +1,8 @@
+const frutas = ['laranja', 'maçã', 'abacaxi','pêra']
+/* O método pop() remove o último elemento de um array e retorna aquele elemento. */
+const frutas2 = frutas.pop()
+console.log(frutas2)
+
+
+/*O array frutas agora está sem o ultimo elemento*/
+console.log(frutas)
