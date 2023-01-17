5518e8351e4ac879c6b4cb7bdf1d75aee7136569
dannielfisico <treinamentosinterativa@gmail.com>
2023-01-17 19:32:37 -0300

    arrays .find()

---
+++ ESTUDOS/arrays/Array.prototype.filter.js
@@ -0,0 +1,4 @@
+/*O método filter() cria um novo array com todos os elementos que passaram no teste implementado pela função fornecida.
+*/
+const frutas = ['laranja', 'maçã', 'abacaxi','pêra','limão','maracujá']
+
---
+++ ESTUDOS/arrays/Array.prototype.find.js
@@ -0,0 +1,11 @@
+/**
+ * O método find() retorna o valor do primeiro elemento do array que satisfizer a função de teste provida. Caso contrario, undefined é retornado.
+ */
+
+const notasDoAluno = [1,8,4,7]
+const baixa1 = notasDoAluno.find(element => element < 7)
+console.log(baixa1)
+
+
+
+const boa1 = notasDoAluno.find(boa)
