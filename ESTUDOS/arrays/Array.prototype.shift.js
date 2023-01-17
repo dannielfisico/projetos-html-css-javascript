082bc152bd3f2736931528a80b5a92ca78e82a33
dannielfisico <treinamentosinterativa@gmail.com>
2023-01-17 15:35:23 -0300

    arrays .shift()

---
+++ ESTUDOS/arrays/Array.prototype.shift.js
@@ -0,0 +1,19 @@
+/*
+O método **shift()**remove o primeiro elemento de um array e retorna esse elemento. Este método muda o tamanho do array. */
+
+const frutas = ['laranja', 'maçã', 'abacaxi','pêra']
+const primeiraFruta = frutas.shift()
+console.log(frutas)
+
+//Usando o método shift() dentro de um loop de while
+const visitantes = ['Daniel', 'João', 'Marcos', 'Jamila', 'Gabriel']
+const clientes = []
+//transferir os visitantes para clientes
+
+while((i = visitantes.shift()) !== undefined){
+    clientes.push(i)
+}
+
+
+console.log(visitantes)
+console.log(clientes)
