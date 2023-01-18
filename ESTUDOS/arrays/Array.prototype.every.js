0bf3cc6c00fa1e3ea68541642a4dd6f212c53b43
dannielfisico <treinamentosinterativa@gmail.com>
2023-01-18 10:55:03 -0300

    arrays .every()

--- ESTUDOS/arrays/Array.prototype.every.js
+++ ESTUDOS/arrays/Array.prototype.every.js
@@ -9,3 +9,23 @@
  
  console.log(idade.every(eMaior18))
 
+const frutas = ['abacate', 'banana', 'abacaxi', 'amora', 'ameixa', 'acerola', 'açaí', 'carambola']
+
+console.log(frutas.every(comecaComA)) 
+console.log(frutas.every(comecaComAouB))
+
+
+function comecaComA(nome){
+    if(nome[0] == 'a' || nome[0] == 'b'){
+        return true
+    } else {
+        return false
+    }
+}
+function comecaComAouB(nome){
+    if(nome[0] == 'a' || nome[0] == 'b'){
+        return true
+    } else {
+        return false
+    }
+}
