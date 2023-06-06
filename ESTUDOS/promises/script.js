10b7ac7732436953f6f647ec02cc44ae8458c207
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-06 12:38:07 -0300

    Promises01 - atualização3

---
+++ ESTUDOS/promises/script.js
@@ -0,0 +1,12 @@
+/**
+ PROMISES
+ 
+ São como promessas da vida real, ela está esperando algum retorno;
+ Para criar uma promessa instanciamos a classe Promise;
+ Que leva dois argumentos: resolve (solução) e reject (erro);
+ Para encadear mais processos usamos o método then;
+ Alguns recursos de JavaScript (Fetch API) e bibliotecas retornam Promises;
+
+ */
+
+ 
