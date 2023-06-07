76107c2dc94f953e7f2149acbe5e13f34e8e33a6
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-07 14:08:28 -0300

    Async e Await - Projeto01

---
+++ ESTUDOS/Async-Await/projeto01/script.js
@@ -0,0 +1,17 @@
+//#region Async e Await (Introdução ao tema)
+/**
+    As funções assíncronas funcionam como Promises, porém com uma sintaxe mais simples;
+    Precisamos declara a função com a palavra async;
+    Enquanto precisamos aguardar por algo da instrução precisa de await;
+    Podemos aplicar o recurso em funções anônimas e métodos de classe;
+    Tentar usar o awayt sem o async gera um erro;
+    Exemplo de uso: inserção de dado no banco;  
+ * */    
+
+//#endregion
+
+//#region 
+
+
+//#endregion
+
