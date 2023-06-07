c8a2c3916a6b2f347487f4f8b33cbabbc362d77d
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-07 11:25:46 -0300

    Promises05 - Projeto Setar Foto GitHub com username (Usando api)- 04

--- ESTUDOS/promises/promises05/script.js
+++ ESTUDOS/promises/promises05/script.js
@@ -14,6 +14,17 @@ document.getElementById('btnBuscar').onclick = () => {
     }).then((dadosDaResposta) => {
         console.log(typeof dadosDaResposta) //a resposta será um objeto
         console.log(dadosDaResposta) //o objeto será impresso no console
-        
+        return dadosDaResposta.json() //converte a resposta em json para que possamos pegar os dados
+    }).then( (dadosDaRespostaJSON) => {
+        console.log(dadosDaRespostaJSON) //imprime o objeto json
+        let local
+            if(dadosDaRespostaJSON.location != null){
+                local = dadosDaRespostaJSON.location
+            } else {
+                local = 'algum lugar não definido.'
+            }
+        document.getElementById('legenda').innerHTML = `${dadosDaRespostaJSON.name} de ${local}`
+        document.getElementById('perfil').src = dadosDaRespostaJSON.avatar_url
+        document.getElementById('nomeUsuario').value = ""
     })
 } 
