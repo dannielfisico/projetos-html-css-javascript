5262efdd506897835476f6574e9351e22c017bbc
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-07 21:48:27 -0300

    Async e Await - Projeto02 parte2 - Requisição api resreq

--- ESTUDOS/Async-Await/projeto02/script.js
+++ ESTUDOS/Async-Await/projeto02/script.js
@@ -5,11 +5,31 @@ function pegarUsuario(id){
     .catch(err => console.log(err))
 }
 
-// async function mostarUsuario(id){
-//     const usuario = await pegarUsuario(id)
-//     console.log(`O usuário é: ${usuario.data.first_name}`)
-// }
+async function mostarUsuario(){
+    
+    try {
+        const identificador = document.getElementById('txtId').value
+        const usuario = await pegarUsuario(identificador)
+        const info = document.getElementById('info')
+        console.log(pegarUsuario(identificador))
+        console.log(`O usuário é: ${usuario.data.first_name}`)
+        document.getElementById("perfil").src = usuario.data.avatar
+        document.getElementById('legenda').innerHTML = `${usuario.data.first_name}`
+        const lista = document.createElement('ul')
+        lista.setAttribute('id','listaOrdenada')
+        info.appendChild(lista)
+        const item = document.createElement('li')
+        lista.appendChild(item)
+        item.innerHTML = `Nome completo: ${usuario.data.first_name} ${usuario.data.last_name}`
+        const item2 = document.createElement('li')
+        lista.appendChild(item2)
+        item2.innerHTML = `E-mail: ${usuario.data.email}`
+        
+    } catch {
+        alert(`Identificador não existente até o momento`)
+    }
+}
 
-// mostarUsuario(3)
+
 
 //#endregion
