0765547676f54fdb081f5221b7b0c42bd4c4e1d7
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-10 22:07:30 -0300

    Jogo da velha - Versão 1.0

---
+++ ESTUDOS/estudos-html/prog02/script/script.js
@@ -0,0 +1,141 @@
+let c1 = undefined
+let c2 = undefined
+let c3 = undefined
+let c4 = undefined
+let c5 = undefined
+let c6 = undefined
+let c7 = undefined
+let c8 = undefined
+let c9 = undefined
+let simbol = "x"
+let player = document.getElementById('player')
+player.innerText= simbol.toUpperCase()
+
+function jogar1(){
+    
+    if (c1 === undefined){
+        document.getElementById('celula-1').innerHTML = simbol
+        if (simbol === "o"){
+            simbol = "x"
+            player.innerText= simbol.toUpperCase()
+        } else {
+            simbol = "o"
+            player.innerText= simbol.toUpperCase()
+        }
+        c1 = "preenchido"
+        
+    } 
+}
+function jogar2(){
+    if (c2 === undefined){
+        document.getElementById('celula-2').innerHTML = simbol
+        if (simbol === "o"){
+            simbol = "x"
+            player.innerText= simbol.toUpperCase()
+        } else {
+            simbol = "o"
+            player.innerText= simbol.toUpperCase()
+        }
+        c2 = "preenchido"
+    }
+
+}
+function jogar3(){
+    if (c3 === undefined){
+        document.getElementById('celula-3').innerHTML = simbol
+        if (simbol === "o"){
+            simbol = "x"
+            player.innerText= simbol.toUpperCase()
+        } else {
+            simbol = "o"
+            player.innerText= simbol.toUpperCase()
+        }
+        c3 = "preenchido"
+    }
+
+}
+function jogar4(){
+    if (c4 === undefined){
+        document.getElementById('celula-4').innerHTML = simbol
+        if (simbol === "o"){
+            simbol = "x"
+            player.innerText= simbol.toUpperCase()
+        } else {
+            simbol = "o"
+            player.innerText= simbol.toUpperCase()
+        }
+        c4 = "preenchido"
+    }
+
+}
+function jogar5(){
+    if (c5 === undefined){
+        document.getElementById('celula-5').innerHTML = simbol
+        if (simbol === "o"){
+            simbol = "x"
+            player.innerText= simbol.toUpperCase()
+        } else {
+            simbol = "o"
+            player.innerText= simbol.toUpperCase()
+        }
+        c5 = "preenchido"
+    }
+
+}
+function jogar6(){
+    if (c6 === undefined){
+        document.getElementById('celula-6').innerHTML = simbol
+        if (simbol === "o"){
+            simbol = "x"
+            player.innerText= simbol.toUpperCase()
+        } else {
+            simbol = "o"
+            player.innerText= simbol.toUpperCase()
+        }
+        c6 = "preenchido"
+    }
+
+}
+function jogar7(){
+    if (c7 === undefined){
+        document.getElementById('celula-7').innerHTML = simbol
+        if (simbol === "o"){
+            simbol = "x"
+            player.innerText= simbol.toUpperCase()
+        } else {
+            simbol = "o"
+            player.innerText= simbol.toUpperCase()
+        }
+        c7 = "preenchido"
+    }
+
+}
+function jogar8(){
+    if (c8 === undefined){
+        document.getElementById('celula-8').innerHTML = simbol
+        if (simbol === "o"){
+            simbol = "x"
+            player.innerText= simbol.toUpperCase()
+        } else {
+            simbol = "o"
+            player.innerText= simbol.toUpperCase()
+        }
+        c8 = "preenchido"
+    }
+
+}
+function jogar9(){
+    if (c9 === undefined){
+        document.getElementById('celula-9').innerHTML = simbol
+        if (simbol === "o"){
+            simbol = "x"
+            player.innerText= simbol.toUpperCase()
+        } else {
+            simbol = "o"
+            player.innerText= simbol.toUpperCase()
+        }
+        c9 = "preenchido"
+    }
+
+}
+
