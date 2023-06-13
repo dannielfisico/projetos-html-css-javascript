f0b79371e9f65faf33fb6b3240fbb5d4a63c221b
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-13 18:09:33 -0300

    Jogo da Velha Full 4 - Quase pronto

--- Projeto-Jogo-da-velha/Jogo_Da-Velha_full4/script.js
+++ Projeto-Jogo-da-velha/Jogo_Da-Velha_full4/script.js
@@ -11,6 +11,14 @@ let avatarPlayer2 = ""
 let avatarPlayer1txt = []
 let avatarPlayer2txt = []
 
+let simbolos = ["", ""]
+
+let jogoComecou = false
+
+let jogadorDaVez = 0
+let qtdJogadas = 0
+let jogadasFeitas = ["","","","","","","","",""]
+
 document.addEventListener('DOMContentLoaded', () => {
 
     avatares1txt.forEach(txtavatar => {
@@ -24,11 +32,18 @@ document.addEventListener('DOMContentLoaded', () => {
             avatarPlayer2txt[d] = avatares2txt[d].innerHTML
         }
     })
+    
 })
 
 const celulas = document.querySelectorAll(".celula")
 
 function jogar(){
+    const board = document.querySelector(".board")
+    board.style.display = "grid"
+    const formulario = document.querySelector('.formulario')
+    formulario.style.display = "none"
+
+
     avatares1rad.forEach(avatar1 => {
         for(var a = 0; a < avatares1rad.length; a++){
             if(avatares1rad[a].checked){
@@ -55,7 +70,14 @@ function jogar(){
     iconesSelecionados[1] = avatarPlayer2
 
     
+    simbolos[0] = avatarPlayer1txt[iconesSelecionados[0]]
+    simbolos[1] = avatarPlayer2txt[iconesSelecionados[1]]
+
+    /* ############## GAME #######################*/
+    //Após o usuário selecionar os avatares dos players e clicar no botão jogar, será dado inico ao jogo
+
     
+
 
     
     //log
@@ -65,7 +87,64 @@ function jogar(){
     console.log(`Avatares Player txt 1 ${avatarPlayer1txt}`)    
     console.log(`Avatares Player txt 2 ${avatarPlayer2txt}`)
     console.log((`Icones Selecionados : ${avatarPlayer1txt[iconesSelecionados[0]]} e ${avatarPlayer2txt[iconesSelecionados[1]]}`))
+    console.log(`Simbolos: ${simbolos}`)
 
-
+    jogoComecou = true
+    startGame()
+    
 }
 
+
+function startGame(){
+    
+    if(jogoComecou){
+        console.log(`O jogo começou kkk`)
+
+        const celulas = document.querySelectorAll('.celula')
+            celulas.forEach(celula => {
+                celula.addEventListener('click', ondeClicou => {
+                    console.log(`Clicou na celula de id=${ondeClicou.target.id}`)
+                    if (jogadasFeitas[ondeClicou.target.id] == ""){
+                        ondeClicou.target.innerHTML = simbolos[jogadorDaVez]
+
+                           
+                    }
+                        if(jogadorDaVez == 0){
+                            jogadorDaVez = 1
+                            qtdJogadas++
+                            tabuleiroPreenchido()
+                        } else {
+                            jogadorDaVez = 0 
+                            qtdJogadas++
+                            tabuleiroPreenchido()
+                        }
+                        
+                })
+            })
+
+
+
+
+
+
+
+
+
+    } else {
+        console.log(`O jogo não começou`)
+    }
+    
+ 
+}
+
+
+function tabuleiroPreenchido(){
+    if(qtdJogadas == 9){
+        const controles = document.querySelector(".formulario")
+        controles.style.display = "block"
+        setTimeout(() => {
+            document.location.reload()
+        }, 1000);
+    }
+
+}
