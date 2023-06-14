b22151d876325ccb6eb6410ffdd303035f4873c3
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-13 22:28:24 -0300

    Jogo da Memória - Criação de interface

--- Projeto-Jogo-da-memoria/jogo-da-memoria-projeto2/scritp/memoria.js
+++ Projeto-Jogo-da-memoria/jogo-da-memoria-projeto2/scritp/memoria.js
@@ -1,7 +1,7 @@
 const card = document.querySelector('.card')
 let flipada = 0
-card.addEventListener('click', ()=> {
-    if(flipada){
+card.addEventListener('click', () => {
+    if (flipada) {
         card.setAttribute('class', "card flip")
         flipada--
     } else {
