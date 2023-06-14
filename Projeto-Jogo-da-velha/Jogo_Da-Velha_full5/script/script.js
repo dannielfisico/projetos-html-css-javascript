6c5812a85c6a3218036766ff7078011d7f681282
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-14 13:20:42 -0300

    Jogo da velha Versão 4 - completo e funcional

--- Projeto-Jogo-da-velha/Jogo_Da-Velha_full5/script/script.js
+++ Projeto-Jogo-da-velha/Jogo_Da-Velha_full5/script/script.js
@@ -3,6 +3,7 @@ let jogadorAtual = 0
 const simbolos = ["X", "O"]
 let idJogada = ""
 let jogadasRealizadas = 0
+let gameOver = false
 
 document.addEventListener("DOMContentLoaded", ()=> {
     const celulas = document.querySelectorAll(".celula")
@@ -11,6 +12,10 @@ document.addEventListener("DOMContentLoaded", ()=> {
             console.log(ondeClicou.target.id)
             idJogada = ondeClicou.target.id
             if(jogadasFeitas[idJogada] == ""){
+                if(gameOver){
+                    return
+                }
+
                 jogadasRealizadas++
                 const txtUltima = document.querySelector(".ultima-jogada")
                 txtUltima.innerHTML = `Última Jogada foi de: ${simbolos[jogadorAtual]}`
@@ -19,15 +24,19 @@ document.addEventListener("DOMContentLoaded", ()=> {
                 jogadasFeitas[idJogada] = simbolos[jogadorAtual]
                 console.log(jogadasFeitas)
                 celula.innerHTML = `${simbolos[jogadorAtual]}`
-
+               
+               if (jogadasRealizadas > 4){
+                 gameOver = eCampeao()
+                 const txtVenvedor = document.querySelector('.resultado')
+                 txtVenvedor.innerHTML = `Vencedor: ${jogadorAtual}`
+               } else {
                 trocarDeJogador()
-                    // if(jogadorAtual == 0){
-                    //     jogadorAtual = 1 //Trocar de Jogador
+               }
+
+                
+               
+                
                     
-                    // } else {
-                    //     jogadorAtual = 0
-                    
-                    // }
             }
 
         })
@@ -43,4 +52,36 @@ function trocarDeJogador(){
         jogadorAtual = 0
     
     }
+}
+
+function fimDoJogo(){
+    return eCampeao()
+}
+
+function eCampeao(){
+
+    const possibilidades = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    for(let i = 0; i < possibilidades.length; i++){
+        let sequencia = possibilidades[i]
+
+        let pos1 = sequencia[0]
+        let pos2 = sequencia[1]
+        let pos3 = sequencia[2]
+
+        if( jogadasFeitas[pos1] == jogadasFeitas[pos2]  && jogadasFeitas[pos2] == jogadasFeitas[pos3] && jogadasFeitas[pos1] != ""){
+            return true
+        }
+    }
+
+    return false
 }
