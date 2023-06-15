9a03d8d7ff23d0c5659cdbfcad618d5aecad0d71
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-14 23:58:08 -0300

    Jogo da Velha full 4 - novas funcionalidades

--- Projeto-Jogo-da-velha/Jogo_Da-Velha_full4/script.js
+++ Projeto-Jogo-da-velha/Jogo_Da-Velha_full4/script.js
@@ -1,25 +1,22 @@
 const avatares1rad = document.getElementsByName("player1")
 const avatares1txt = document.querySelectorAll('#icone1')
-
 const avatares2rad = document.getElementsByName("player2")
 const avatares2txt = document.querySelectorAll('#icone2')
-
 const iconesSelecionados = ["",""]
-
 let avatarPlayer1 = ""
 let avatarPlayer2 = ""
 let avatarPlayer1txt = []
 let avatarPlayer2txt = []
-
 let simbolos = ["", ""]
-
 let jogoComecou = false
-
+let gameOver = false
 let jogadorDaVez = 0
 let qtdJogadas = 0
 let jogadasFeitas = ["","","","","","","","",""]
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    const celulas = document.querySelectorAll(".celula")
 
     avatares1txt.forEach(txtavatar => {
         for(let c = 0; c < avatares1txt.length; c++){
@@ -35,21 +32,19 @@ document.addEventListener('DOMContentLoaded', () => {
     
 })
 
-const celulas = document.querySelectorAll(".celula")
 
 function jogar(){
-    const board = document.querySelector(".board")
-    board.style.display = "grid"
-    const formulario = document.querySelector('.formulario')
-    formulario.style.display = "none"
 
-
+    const board = document.querySelector(".board") //seleciona o tabuleiro
+    board.style.display = "grid"    //exibe o tabuleiro colocando do diplay em grid
+    const formulario = document.querySelector('.formulario') //seleciona o formulario 
+    formulario.style.display = "none" //oculta o fomulario
+    document.querySelector('.info').style.display = "block"
+    
     avatares1rad.forEach(avatar1 => {
         for(var a = 0; a < avatares1rad.length; a++){
             if(avatares1rad[a].checked){
                 avatarPlayer1 = a
-                
-                
            }
         
         }
@@ -60,8 +55,6 @@ function jogar(){
         for(var b = 0; b < avatares2rad.length; b++){
            if(avatares2rad[b].checked){
                 avatarPlayer2 = b
-                
-                
            }
         }
     })
@@ -72,7 +65,9 @@ function jogar(){
     
     simbolos[0] = avatarPlayer1txt[iconesSelecionados[0]]
     simbolos[1] = avatarPlayer2txt[iconesSelecionados[1]]
-
+    document.querySelector("#avatar1").innerHTML = `${simbolos[0]}`
+    document.querySelector("#avatar2").innerHTML = `${simbolos[1]}`
+    
     /* ############## GAME #######################*/
     //Após o usuário selecionar os avatares dos players e clicar no botão jogar, será dado inico ao jogo
 
@@ -103,32 +98,35 @@ function startGame(){
         const celulas = document.querySelectorAll('.celula')
             celulas.forEach(celula => {
                 celula.addEventListener('click', ondeClicou => {
+                    if(gameOver){
+                        return
+                    }
+
                     console.log(`Clicou na celula de id=${ondeClicou.target.id}`)
                     if (jogadasFeitas[ondeClicou.target.id] == ""){
-                        ondeClicou.target.innerHTML = simbolos[jogadorDaVez]
-
-                           
-                    }
-                        if(jogadorDaVez == 0){
-                            jogadorDaVez = 1
-                            qtdJogadas++
-                            tabuleiroPreenchido()
-                        } else {
-                            jogadorDaVez = 0 
-                            qtdJogadas++
-                            tabuleiroPreenchido()
-                        }
                         
+
+                        ondeClicou.target.innerHTML = simbolos[jogadorDaVez]
+                        jogadasFeitas[ondeClicou.target.id] = simbolos[jogadorDaVez]
+                        eCampeao()   
+                    }
+
+                    if(!eCampeao()){
+                        passarJogada()
+                    }
+                    
+                    gameOver = eCampeao()
+
+                    if(gameOver){
+                        document.querySelector('.resultado').innerHTML = `${simbolos[jogadorDaVez]} VENCEU.`
+                        document.querySelector('.hidden').style.display = "block"
+                        document.querySelector('.hidden').addEventListener("click", ()=>{
+                            document.location.reload()
+                        })
+
+                    }
                 })
             })
-
-
-
-
-
-
-
-
 
     } else {
         console.log(`O jogo não começou`)
@@ -137,6 +135,19 @@ function startGame(){
  
 }
 
+
+function passarJogada(){
+    if(jogadorDaVez == 0){
+        jogadorDaVez = 1
+        qtdJogadas++
+        // tabuleiroPreenchido()
+    } else {
+        jogadorDaVez = 0 
+        qtdJogadas++
+        // tabuleiroPreenchido()
+    }
+    
+}
 
 function tabuleiroPreenchido(){
     if(qtdJogadas == 9){
@@ -147,4 +158,31 @@ function tabuleiroPreenchido(){
         }, 1000);
     }
 
+}
+
+function eCampeao(){
+    const estadoVitoria = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    for(let i = 0; i < estadoVitoria.length; i++){
+        let sequencia = estadoVitoria[i]
+
+        let p1 = sequencia[0]
+        let p2 = sequencia[1]
+        let p3 = sequencia[2]
+
+        if(jogadasFeitas[p1] == jogadasFeitas[p2] && jogadasFeitas[p2] == jogadasFeitas[p3] && jogadasFeitas[p1] != ""){
+            return true
+        }
+     }
+
+     return false
 }
