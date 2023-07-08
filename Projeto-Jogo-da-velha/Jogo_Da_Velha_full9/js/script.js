7fb8dd6488885c39350873e052af01212c17309c
dannielfisico <treinamentosinterativa@gmail.com>
2023-07-07 23:49:08 -0300

    criação das variáveis de ambiente

--- Projeto-Jogo-da-velha/Jogo_Da_Velha_full9/js/script.js
+++ Projeto-Jogo-da-velha/Jogo_Da_Velha_full9/js/script.js
@@ -2,4 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //criação das variáveis do jogo
     const tabuleiro = []
     const simbolos = ["❌","⭕"]
+    let jogadorAtual = 0
+
+    
 })
