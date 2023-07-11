bf10005e957078c26480faadfd81d2473902e970
dannielfisico <treinamentosinterativa@gmail.com>
2023-07-11 20:50:45 -0300

    html avançado

--- DOM23/script/script.js
+++ DOM23/script/script.js
@@ -1,25 +1,51 @@
 document.addEventListener('DOMContentLoaded',()=>{
     let posicao = localStorage.length
-    let nomeAlunos = []
-    for(i = 0; i < localStorage.length; i++){
-        nomeAlunos[i] = localStorage.getItem(`Aluno${i+1}`)
-    }
+    const nomeAlunos = []
+    const lista = document.querySelector('.lista')
+
+
+
+        for(i = 0; i < localStorage.length; i++){
+            nomeAlunos[i] = localStorage.getItem(`Aluno${i+1}`)
+            lista.innerHTML += `<li>${i+1}.${nomeAlunos[i]}</li> `
+            
+        }
     const btnSalvar = document.querySelector('#btnSalvar')
     
     const totalAlunos = document.querySelector('#total')
     totalAlunos.innerHTML = localStorage.length
-    console.log(nomeAlunos)
+    console.log(localStorage.length)
+    const texto = document.querySelector('#aluno')
+    texto.addEventListener('input', () => {
+        let situation = true
+        if(texto.value.length >=3 && texto.value.trim() != ""){
+            situation = false
+            btnSalvar.disabled = situation
+            situation = true
+        } else {
+            btnSalvar.disabled = situation
+        }
+        
+    })
+    
+    
+
     function salvarAluno(e){
         const aluno = document.querySelector('#aluno')
         if(aluno.value.length >=3){
+            
+            if(localStorage.length >=30){
+                alert('O máximo de alunos nessa turma é 30')
+                aluno.value = ""
+                return
+            }
             // console.log(aluno.value)
             if(nomeAlunos.indexOf(aluno.value) == -1){
                 nomeAlunos[posicao] = aluno.value
                 console.log(nomeAlunos)
                 posicao++
                 localStorage.setItem(`Aluno${posicao}`, aluno.value)
-                
-                
+                lista.innerHTML += `<li>${posicao}.${nomeAlunos[posicao-1]}</li> `                
             } else {
                 alert('Já cadastrado')
             }
@@ -28,8 +54,10 @@ document.addEventListener('DOMContentLoaded',()=>{
             totalAlunos.innerHTML = localStorage.length
             aluno.value = ""
         }
-
+        aluno.focus()
         
     }
     btnSalvar.addEventListener('click', salvarAluno)
+
+    
 })
