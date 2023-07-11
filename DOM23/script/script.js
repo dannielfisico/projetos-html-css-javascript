170e5a587e33b4bb672ca153a6780fa9418809c9
dannielfisico <treinamentosinterativa@gmail.com>
2023-07-11 18:20:30 -0300

    html avançado

--- DOM23/script/script.js
+++ DOM23/script/script.js
@@ -1,13 +1,32 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    let alunos = 0
+    let posicao = localStorage.length
+    let nomeAlunos = []
+    for(i = 0; i < localStorage.length; i++){
+        nomeAlunos[i] = localStorage.getItem(`Aluno${i+1}`)
+    }
     const btnSalvar = document.querySelector('#btnSalvar')
-
+    
+    const totalAlunos = document.querySelector('#total')
+    totalAlunos.innerHTML = localStorage.length
+    console.log(nomeAlunos)
     function salvarAluno(e){
         const aluno = document.querySelector('#aluno')
         if(aluno.value.length >=3){
-            console.log(aluno.value) 
-        e.preventDefault()
-        aluno.value = ""
+            // console.log(aluno.value)
+            if(nomeAlunos.indexOf(aluno.value) == -1){
+                nomeAlunos[posicao] = aluno.value
+                console.log(nomeAlunos)
+                posicao++
+                localStorage.setItem(`Aluno${posicao}`, aluno.value)
+                
+                
+            } else {
+                alert('Já cadastrado')
+            }
+            
+            e.preventDefault()
+            totalAlunos.innerHTML = localStorage.length
+            aluno.value = ""
         }
 
         
