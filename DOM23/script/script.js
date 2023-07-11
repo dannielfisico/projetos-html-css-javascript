f4c9d3f9cc0ea3fb12105a2d305926f58e149ae1
dannielfisico <treinamentosinterativa@gmail.com>
2023-07-11 16:26:35 -0300

    html avançado

--- DOM23/script/script.js
+++ DOM23/script/script.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded',()=>{
     let alunos = 0
-    const btnSalvar = document.querySelector('btnSalvar')
+    const btnSalvar = document.querySelector('#btnSalvar')
 
-    btnSalvar.addEventListener('click', salvarAluno)
+    function salvarAluno(e){
+        const aluno = document.querySelector('#aluno')
+        console.log(aluno) 
+        e.preventDefault()
+    }
+    btnSalvar.addEventListener('click', salvarAluno,  )
 })
