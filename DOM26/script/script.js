caac044ebe928366e0fd6d3b3e19c583ac1cbaad
dannielfisico <treinamentosinterativa@gmail.com>
2023-07-25 13:59:52 -0300

    Projeto CRUD - LOCALSTORAGE

--- DOM26/script/script.js
+++ DOM26/script/script.js
@@ -1,41 +1,55 @@
-console.log('página carregada com sucesso...')
+console.log("página carregada com sucesso...");
 
-    //Funções
+//Funções
 
-    const exibirOcultarModal = () => {
-        modalOverlay.classList.toggle('oculto')
-    }
+const exibirOcultarModal = () => {
+  modalOverlay.classList.toggle("oculto");
+};
 
+//Botões
+const btnCadastrar = document.querySelector(".btnCadastrar");
+const btnFechar = document.querySelector("#btnFechar");
 
-    //Botões
-    const btnCadastrar = document.querySelector('.btnCadastrar')
-    const btnFechar = document.querySelector("#btnFechar")
-    
+//Modal Overlay
+const modalOverlay = document.querySelector(".overlay");
 
-    //Modal Overlay
-    const modalOverlay = document.querySelector('.overlay')
+//Evento exibir Modal Overlay
 
-    //Evento exibir Modal Overlay
+btnCadastrar.addEventListener("click", exibirOcultarModal);
+btnFechar.addEventListener("click", exibirOcultarModal);
 
-    btnCadastrar.addEventListener('click', exibirOcultarModal)
-    btnFechar.addEventListener('click', exibirOcultarModal)
+// TempCliente
+const tempCliente = {
+  nome: "Paulo Rangel",
+  email: "paulorangel@gmail.com",
+  profissoao: "Ciclista Profissional",
+};
+// CRUD
+//C - CREATE Criar registros no LocalStorage
+const setLocalStorage = (dataBase) =>
+  localStorage.setItem("dataBase", JSON.stringify(dataBase));
+//R - READ - Ler registros do LocalStorage
+const getLocalStorage = () =>
+  JSON.parse(localStorage.getItem("dataBase")) ?? [];
+//U - UPDATE  - Atualizar registro no LocalStorage
+const updateClient = (index, client) => {
+  const dataBase = getLocalStorage();
+  dataBase[index] = client;
+  setLocalStorage(dataBase);
+};
+//D - DELETE - Apaga registro específico no LocalStorage
+const deleteClient = (index, qtd) => {
+  const dataBase = getLocalStorage();
+  dataBase.splice(index, qtd);
+  setLocalStorage(dataBase)
+};
 
-    // TempCliente
+const createClient = (client) => {
+  const dataBase = getLocalStorage();
+  dataBase.push(client);
+  setLocalStorage(dataBase);
+};
 
-    const tempCliente = {
-        nome: 'Fernando Nonato',
-        email: 'fernandononato@gmail.com',
-        profissoao: 'Comerciante'
-    }
-
-    //Ler registros do LocalStorage
-
-    
-
-    //Salvar Registro
-
-    
-
-    
-
-    
+// createClient(tempCliente)
+// updateClient(5,tempCliente)
+// deleteClient(4, 1);
