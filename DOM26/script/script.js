07e2dc3315b3c9289246e018fa29d6244ae88a87
dannielfisico <treinamentosinterativa@gmail.com>
2023-07-25 16:18:02 -0300

    Projeto CRUD localstorage

--- DOM26/script/script.js
+++ DOM26/script/script.js
@@ -1,32 +1,5 @@
 console.log("página carregada com sucesso...");
 
-//Funções de exibição dos Modais
-
-const exibirOcultarModal = () => {
-  modalOverlay.classList.toggle("oculto");
-};
-
-const exibirOcultarModalExcluir = () => {
-  modalExcluir.classList.toggle("oculto");
-};
-
-//Botões
-const btnCadastrar = document.querySelector(".btnCadastrar");
-const btnFechar = document.querySelector("#btnFechar");
-const btnCancelar = document.querySelector('#btnCancelar')
-const btnFecharModalExluir = document.querySelector('#btnFecharModalExluir')
-
-//Modal Overlay
-const modalOverlay = document.querySelector(".overlay");
-//Modal Excluir
-const modalExcluir = document.querySelector('.modal-excluir')
-
-//Evento exibir Modal Overlay
-
-btnCadastrar.addEventListener("click", exibirOcultarModal);
-btnFechar.addEventListener("click", exibirOcultarModal);
-btnCancelar.addEventListener('click', exibirOcultarModalExcluir)
-btnFecharModalExluir.addEventListener('click', exibirOcultarModalExcluir)
 
 // TempCliente
 const tempCliente = {
@@ -60,6 +33,34 @@ const createClient = (client) => {
   setLocalStorage(dataBase);
 };
 
-// createClient(tempCliente)
-// updateClient(5,tempCliente)
-// deleteClient(4, 1);
+// INTERAÇÃO COM A INTERFACE DO USUÁRIO
+
+          //Funções de exibição dos Modais
+
+const exibirOcultarModal = () => {
+  modalOverlay.classList.toggle("oculto");
+};
+
+const exibirOcultarModalExcluir = () => {
+  modalExcluir.classList.toggle("oculto");
+};
+
+//Botões
+const btnCadastrar = document.querySelector(".btnCadastrar");
+const btnFechar = document.querySelector("#btnFechar");
+const btnCancelar = document.querySelector('#btnCancelar')
+const btnFecharModalExluir = document.querySelector('#btnFecharModalExluir')
+
+//Modal Overlay
+const modalOverlay = document.querySelector(".overlay");
+//Modal Excluir
+const modalExcluir = document.querySelector('.modal-excluir')
+
+//Evento exibir Modal Overlay
+
+btnCadastrar.addEventListener("click", exibirOcultarModal);
+btnFechar.addEventListener("click", exibirOcultarModal);
+btnCancelar.addEventListener('click', exibirOcultarModalExcluir)
+btnFecharModalExluir.addEventListener('click', exibirOcultarModalExcluir)
+
+
