e0ca0bfb70f3be21274736aa9b871bf0ba344d6a
dannielfisico <treinamentosinterativa@gmail.com>
2023-08-07 14:01:37 -0300

    jogo da memoria4 - script

--- Projeto-Jogo-da-memoria/jogo-da-memoria-projeto4/js/game.js
+++ Projeto-Jogo-da-memoria/jogo-da-memoria-projeto4/js/game.js
@@ -1,9 +1,13 @@
-const celulas = document.querySelectorAll('.square')
+const tabuleiro = ["", "", "", "", "", "", "", "", ""];
+const simbolos = ["X", "O"];
+let jogador = 0;
 
-
+const celulas = document.querySelectorAll(".square");
 
 celulas.forEach((celula) => {
-    celula.addEventListener('click', (e) => {
-        e.target = celula.innerHTML = 'x'
-    })
-})
+  celula.addEventListener("click", (e) => {
+    tabuleiro[e.target.dataset.i] = simbolos[jogador]
+    e.target = celula.innerHTML = simbolos[jogador];
+    jogador = jogador == 0 ? 1:  0;
+  },{once: true}) ;
+});
