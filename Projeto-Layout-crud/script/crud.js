20ad84006d77a60025dc860beec5e2e7c093b785
dannielfisico <treinamentosinterativa@gmail.com>
2023-08-11 18:27:50 -0300

    script crud

--- Projeto-Layout-crud/script/crud.js
+++ Projeto-Layout-crud/script/crud.js
@@ -1,7 +1,15 @@
 
+const btnCadastrar = document.getElementsByClassName('btnCadastrar')[0]
+const btnCancelar = document.getElementsByClassName('btnCancelar')[0]
+const modalClose = document.getElementsByClassName('modal-close')[0]
 const modal = document.getElementById('modal')
 
 const exibirOcultarModal = () => {
     modal.classList.toggle('oculto')
 }
 
+//Eventos
+
+btnCadastrar.addEventListener('click', exibirOcultarModal)
+btnCancelar.addEventListener('click', exibirOcultarModal)
+modalClose.addEventListener('click', exibirOcultarModal)
